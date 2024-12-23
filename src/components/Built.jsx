31faeaa19872985c1a50dba5f8202858f167be00
mike489 <michaeltesfaye84@yahoo.com>
2024-12-23 10:48:49 +0300

    adding the resposives on the web

--- src/components/Built.jsx
+++ src/components/Built.jsx
@@ -1,4 +1,3 @@
-// import React from 'react'
 import { useState } from "react";
 import Admin from "./Experts/Admin";
 import HelpDesk from "./Experts/HelpDesk";
@@ -29,26 +28,25 @@ const Built = () => {
   return (
     <article className="bg-white">
       <div className="flex flex-col items-start justify-center px-4 sm:px-6 lg:px-20">
-        <h1 className="mt-24 text-secondary font-bold text-xl sm:text-2xl md:text-3xl text-start">
+        <h1 className="mt-24 text-secondary font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl text-start">
           Solutions for Every Team
         </h1>
-        <h2 className="mt-10 w-1/2 text-[#050038B2] font-light  text-start">
+        <h2 className="mt-10 w-full sm:w-3/4 lg:w-1/2 text-[#050038B2] font-light text-sm sm:text-base md:text-lg text-start">
           Whether you’re brainstorming, engineering, managing workshops, or
           engaging with customers, Yam Consulting Service provides the tools to
-          elevate your team&apos;s performance
+          elevate your team&apos;s performance.
         </h2>
-        <div className="w-full flex flex-wrap items-start justify-start mt-8 gap-4 sm:gap-6 lg:gap-10">
+        <div className="w-full flex flex-wrap items-start justify-center sm:justify-start mt-8 gap-3 sm:gap-6 lg:gap-10">
           {[
-            { name: "Brainstorming", label: "Brainstorming" },
+            { name: "Admin", label: "Brainstorming" },
             { name: "HelpDesk", label: "Engineering" },
             { name: "TeamLead", label: "Workshops" },
             { name: "Customer", label: "Customer Engagement" },
-            // { name: "ScrumEvents", label: "ScrumEvents" },
           ].map((button) => (
             <button
               key={button.name}
               onClick={() => setSelectedComponent(button.name)}
-              className={`p-2 rounded-full pl-10 pr-10 font-normal border  ${
+              className={`px-6 py-2 rounded-full font-normal border text-sm sm:text-base ${
                 selectedComponent === button.name
                   ? "bg-[#F2F2F2] text-secondary"
                   : "bg-white text-[#767676]"
@@ -59,7 +57,7 @@ const Built = () => {
           ))}
         </div>
       </div>
-      <div className="mt-5 px-4 sm:px-6 ">{renderComponent()}</div>
+      <div className="mt-5 px-4 sm:px-6">{renderComponent()}</div>
     </article>
   );
 };
--- src/components/Cloudy.jsx
+++ src/components/Cloudy.jsx
@@ -2,7 +2,7 @@ import Cloudyimg from "../assets/img/cloudy.png";
 
 const Cloudy = () => {
   return (
-    <article className="relative flex justify-center items-center w-full  px-[10%]">
+    <article className="relative flex justify-center items-center w-full px-4 sm:px-8 lg:px-[10%]">
       {/* Image */}
       <img
         src={Cloudyimg}
@@ -10,18 +10,22 @@ const Cloudy = () => {
         className="w-full h-full object-cover rounded-t-3xl"
       />
 
-      <div className="absolute right-10 top-1/2 transform text-secondary -translate-y-1/2 text-right space-y-4 px-[8%]">
-        <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-4xl font-bold  ml-auto">The Yam Way</h1>
-          <h1 className="text-4xl font-bold text-center ml-auto">We Work</h1>
-          <p className="text-lg w-[40%] text-start ml-auto">
+      <div className="absolute inset-0 flex flex-col justify-center items-end px-4 sm:px-8 lg:px-[11%] space-y-4">
+        <div className="text-center sm:text-right space-y-4">
+          <h1 className="text-2xl sm:text-xl md:text-2xl lg:text-4xl font-bold md:mr-36 mr-0">
+            The Yam Way
+          </h1>
+          <h1 className="text-2xl sm:text-xl lg:text-4xl font-bold md:mr-44 mr-0">
+            We Work
+          </h1>
+          <p className="text-sm sm:text-sms lg:text-lg ml-auto md:w-1/2 sm:w-1/2 md:text-start sm:items-center">
             Our philosophy revolves around innovation, collaboration, and
             customer-centric solutions. We blend cutting-edge technology with a
             personalized approach, helping businesses thrive in a competitive
             digital landscape.
           </p>
         </div>
-        <button className="bg-primary p-3 rounded-full text-white font-medium px-8 mr-20">
+        <button className="bg-primary p-2 md:p-2 sm:p-1 rounded-full text-white font-medium px-6 sm:px-5 md:mr-40">
           View the Report
         </button>
       </div>
